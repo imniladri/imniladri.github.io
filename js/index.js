eebd6d274c3bfc1b8b4072d20eabc59447f7c7ae
imniladri <jnvniladri101@gmail.com>
2021-05-06 21:36:22 +0530

    Updated Files

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+// Loader
+
+$(window).on("load", function () {
+    $("#preloader").css({
+        transform: "translateY(-100%)",
+        "transition-delay": "0.6s",
+    });
+    $(".loader").css({
+        opacity: "0",
+        transform: "translate(0,-50%)",
+        "transition-delay": "0.3s",
+    });
+});
+
 // AOS.init();
