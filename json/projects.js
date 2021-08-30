610d22fc31a2019754c91aff81681bcafb75955d
imniladri <jnvniladri101@gmail.com>
2021-08-30 13:19:30 +0530

    Used Mapping for Data

---
+++ json/projects.js
@@ -0,0 +1,195 @@
+// All Projects Data Object ****************************************************
+
+let projectsData = [
+    // {
+    //     id: 1,
+    //     name: "Developer Kit",
+    //     details: "A Famous Developer Kit contains all Libraries.",
+    //     imgsrc: "./img/projects/icon1.png",
+    //     preview: "https://imniladri.github.io/DeveloperKit/",
+    //     code: "https://github.com/imniladri/DeveloperKit",
+    //     feedback: "Feedback_on_Developer_Kit",
+    //     icon: "bx-show",
+    // },
+    {
+        id: 1,
+        name: "My Kit",
+        details: "A Famous Developer Kit contains all Libraries.",
+        imgsrc: "./img/projects/icon1.png",
+        preview: "https://imniladri.github.io/MyKit/",
+        code: "https://github.com/imniladri/MyKit",
+        feedback: "Feedback_on_Developer_Kit",
+        icon: "bx-show",
+    },
+    {
+        id: 2,
+        name: "Saac Jgec",
+        details: "Version 2.0 for Official Website of SAAC.",
+        imgsrc: "./img/projects/icon2.png",
+        preview: "https://saacjgec.vercel.app/",
+        code: "https://github.com/imniladri/SaacJgec",
+        feedback: "Feedback_on_Saac_Jgec",
+        icon: "bx-show",
+    },
+    {
+        id: 3,
+        name: "Food Mate",
+        details: "A Basic Project for Food Items & Add to Cart.",
+        imgsrc: "./img/projects/icon3.png",
+        preview: "https://imniladri.github.io/FoodMate/",
+        code: "https://github.com/imniladri/FoodMate",
+        feedback: "Feedback_on_Food_Mate",
+        icon: "bx-show",
+    },
+    {
+        id: 4,
+        name: "Timescape",
+        details: "Awesome Beautiful Clock App, as Timescape.",
+        imgsrc: "./img/projects/icon4.png",
+        preview: "https://imniladri.github.io/Timescape/",
+        code: "https://github.com/imniladri/Timescape",
+        feedback: "Feedback_on_Timescape",
+        icon: "bx-show",
+    },
+    {
+        id: 5,
+        name: "PDF Chest UI",
+        details: "Awesome UI for PDF Chest App in Play Store.",
+        imgsrc: "./img/projects/icon5.png",
+        preview: "https://imniladri.github.io/PDFChestUI/",
+        code: "https://github.com/imniladri/PDFChestUI",
+        feedback: "Feedback_on_PDF_Chest_UI",
+        icon: "bx-show",
+    },
+    {
+        id: 6,
+        name: "We Are 8",
+        details: "Friendly Site of Me & My Friends: Memories.",
+        imgsrc: "./img/projects/icon6.png",
+        preview: "https://imniladri.github.io/WeAre8/",
+        code: "https://github.com/imniladri/WeAre8",
+        feedback: "Feedback_on_We_Are_8",
+        icon: "bx-show",
+    },
+    {
+        id: 7,
+        name: "Front End Apps",
+        details: "Basic Applications used in daily life in Web App.",
+        imgsrc: "./img/projects/icon7.png",
+        preview: "https://imniladri.github.io/FrontEndApps/",
+        code: "https://github.com/imniladri/FrontEndApps",
+        feedback: "Feedback_on_Front_End_Apps",
+        icon: "bx-link",
+    },
+    {
+        id: 8,
+        name: "Educational Site",
+        details: "Beautiful Educational Site for New Educators.",
+        imgsrc: "./img/projects/icon8.png",
+        preview: "https://imniladri.github.io/Educational_Site/",
+        code: "https://github.com/imniladri/Educational_Site",
+        feedback: "Feedback_on_Educational_Site",
+        icon: "bx-show",
+    },
+    {
+        id: 9,
+        name: "Restaurant & Cafe",
+        details: "Branded Cafe for New Openers Startup Site.",
+        imgsrc: "./img/projects/icon9.png",
+        preview: "https://imniladri.github.io/Restaurant_Cafe/",
+        code: "https://github.com/imniladri/Restaurant_Cafe",
+        feedback: "Feedback_on_Cafe_Site",
+        icon: "bx-show",
+    },
+    {
+        id: 10,
+        name: "Portfolio Template",
+        details: "A prebuild portfolio template for new users site.",
+        imgsrc: "./img/projects/icon10.png",
+        preview: "https://awesome-portfolio-template.vercel.app/",
+        code: "https://github.com/imniladri/Productive-Portfolio-Template",
+        feedback: "Feedback_on_Portfolio_Template",
+        icon: "bx-show",
+    },
+    {
+        id: 11,
+        name: "Creative Library",
+        details: "A mini library for arrangement of books or etc.",
+        imgsrc: "./img/projects/icon11.png",
+        preview: "https://imniladri.github.io/Creative_Library/",
+        code: "https://github.com/imniladri/Creative_Library",
+        feedback: "Feedback_on_Books_Library",
+        icon: "bx-show",
+    },
+    {
+        id: 12,
+        name: "Just 4 Fun",
+        details: "Another site of mine! Made just 4 fun :)",
+        imgsrc: "./img/projects/icon12.png",
+        preview: "https://myselfnm.netlify.app/",
+        code: "javascript:void(0);",
+        feedback: "Feedback_on_Just_4_Fun",
+        icon: "bx-show",
+    },
+    {
+        id: 13,
+        name: "Hamburger Bar's",
+        details: "A creative hamburger menus with cool animations.",
+        imgsrc: "./img/projects/icon13.png",
+        preview: "https://imniladri.github.io/HamburgerMenu/",
+        code: "https://github.com/imniladri/HamburgerMenu",
+        feedback: "Feedback_on_Hamburger_Bars",
+        icon: "bx-link",
+    },
+    {
+        id: 14,
+        name: "Preloader's Pack",
+        details: "A good collection of preloaders animations.",
+        imgsrc: "./img/projects/icon14.png",
+        preview: "https://imniladri.github.io/PreloaderEffects/",
+        code: "https://github.com/imniladri/PreloaderEffects",
+        feedback: "Feedback_on_Preloader_Effects",
+        icon: "bx-link",
+    },
+    {
+        id: 15,
+        name: "Info Snippets",
+        details: "Some useful snippets for websites properties.",
+        imgsrc: "./img/projects/icon15.png",
+        preview: "javascript:void(0);",
+        code: "https://github.com/imniladri/InformativeSnippets",
+        feedback: "Feedback_on_Info_Snippets",
+        icon: "bx-link",
+    },
+    {
+        id: 16,
+        name: "Ratings System",
+        details: "Custom ratings builded with cool emoji faces.",
+        imgsrc: "./img/projects/icon16.png",
+        preview: "https://imniladri.github.io/RatingsSystem/",
+        code: "https://github.com/imniladri/RatingsSystem",
+        feedback: "Feedback_on_Ratings_System",
+        icon: "bx-show",
+    },
+];
+
+// All Projects Preview Mapping ****************************************************
+
+let projectsContent = document.querySelector(".projects-wrapper");
+
+Object.values(projectsData).map((val) => {
+    projectsContent.innerHTML += `
+        <!-- Project ${val.id} -->
+        <div class="projects-cards col-xl-3 col-lg-4 col-md-5 col-sm-8 col-12">
+			<img src="${val.imgsrc}" />
+			<h4>${val.name}</h4>
+			<p>${val.details}</p>
+
+			<span class="preview-links">
+                <a href="${val.preview}"><i class="bx ${val.icon}"></i></a>
+                <a href="${val.code}"><i class="bx bx-code-alt"></i></a>
+                <a href="mailto:imniladrimondal@gmail.com?subject=${val.feedback}"><i class="bx bxs-edit"></i></a>
+            </span>
+        </div>
+    `;
+});
